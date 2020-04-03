a1a59782eb35ef87ee34dded5e36186d7887bd6a
Exequiel <exe.gonzalez14@hotmail.com>
2020-04-02 21:11:26 -0300

    add docker files

--- geolocation/backend/api/connectionDB.py
+++ geolocation/backend/api/connectionDB.py
@@ -2,7 +2,7 @@ import redis
 
 def client():
     """Crear conexion a la base de datos."""
-    conexion = redis.StrictRedis(host='localhost',port=6379,db=0, decode_responses=True)
+    conexion = redis.StrictRedis(host='redis',port=6379,db=0, decode_responses=True)
     if(conexion.ping()):
         print("Conectado al servidor de redis")
     else:
